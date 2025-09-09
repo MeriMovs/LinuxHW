5149f4530af2a28542ecc97e5240af272a6f6294
MeriMovs <myblackbounty@gmail.com>
2025-09-09 21:36:58 +0400

    fix

--- ENV-1.2/maxMin.cpp
+++ ENV-1.2/maxMin.cpp
@@ -2,11 +2,25 @@
 
 #include <iostream>
 #include <algorithm>
+#include <regex>
 
 int main(int argc, char** argv){
-	int a = std::atoi(argv[1]);
-    int b = std::atoi(argv[2]);
-    int c = std::atoi(argv[3]);
+	if(argc != 4){
+		std::cerr << "Usage: " << argv[0] << " num1 num2 num3" << std::endl;
+		return 1;
+	}
+
+	bool isAnum = std::regex_search( argv[1], std::regex( "\\D" )); 
+	bool isBnum = std::regex_search( argv[2], std::regex( "\\D" )); 
+	bool isCnum = std::regex_search( argv[3], std::regex( "\\D" ));
+	
+	if( isAnum || isBnum || isCnum ) {
+		std::cerr << "Not a num" << std::endl;
+        return 1;
+	}
+	int a = std::stoi(argv[1]);
+    int b = std::stoi(argv[2]);
+    int c = std::stoi(argv[3]);
 
     int min = std::min({a, b, c});
     int max = std::max({a, b, c});
