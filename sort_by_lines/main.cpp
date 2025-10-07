9b17d7bf9d8165cbad2b02080d63fc6487f0944b
MeriMovs <myblackbounty@gmail.com>
2025-10-08 00:56:07 +0400

    v2

--- sort_by_lines/main.cpp
+++ sort_by_lines/main.cpp
@@ -4,41 +4,34 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
-
-int open_fd(int name_index) {
-    char filename[20];
-    snprintf(filename, sizeof(filename), "%d.txt", name_index);
-    int fd = open(filename, O_RDWR);
-    if (fd == -1) {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
+bool isFileEnd(int fd) {
+    char buffer[1];
+    if (read(fd, buffer, 1) <= 0) {
+        return true;
     }
-    return fd;
+    return false;
 }
 
-std::string read_from(int fd) {
-    std::string res;
-    char bf[4096];
-    ssize_t bytes_read = read(fd, bf, sizeof(bf));
-    while (bytes_read > 0) {
-        res.append(bf, bytes_read);
-        bytes_read = read(fd, bf, sizeof(bf));
+std::string getLine(int fd) {
+    char buffer[1];
+    std::string line;
+    while (read(fd, buffer, 1) > 0) {
+        line += buffer[0];
+        if (buffer[0] == '\n') {
+            return line;  
+        }
     }
-    if (bytes_read == -1) {
-        perror("Error reading from file");
-        exit(EXIT_FAILURE);
-    }
-
-    // std::cout << "read " << res << "\n";
-    return res;
+    return line;
 }
 
-void write_to(int fd, const std::string& str, bool del = true) {
+void write_to(int fd, const std::string& str, bool del = false) {
   if(del) {
     lseek(fd, 0, SEEK_SET);
     ftruncate(fd, 0);
   }
+  if(str == "\n") return;
   ssize_t bytes_written = write(fd, str.c_str(), str.size());
   if (bytes_written == -1) {
       perror("Error writing to file");
@@ -47,79 +40,67 @@ void write_to(int fd, const std::string& str, bool del = true) {
 }
 
 int main() {
-    int fd = open("text", O_RDWR);
+    int fd = open("short_text", O_RDWR);
     if (fd == -1) {
         perror("Error opening file");
         exit(EXIT_FAILURE);
     }
 
-    char filename[20];
-    int i = 0;
-    snprintf(filename, sizeof(filename), "%d.txt", i);
-    int fd2 = open(filename, O_WRONLY | O_CREAT, 0664);
-    if (fd2 == -1) {
+    int fd_3 = open("tmp.txt", O_RDWR | O_CREAT, 0644);
+    if (fd_3 == -1) {
         perror("Error opening file");
         exit(EXIT_FAILURE);
     }
 
-    char buffer[1];
-    std::string line;
-    while (read(fd, buffer, 1) > 0) {
-        line += buffer[0];
-        if (buffer[0] == '\n') {
-            write(fd2, line.c_str(), line.size());
-            close(fd2);
-            line.clear();
-            snprintf(filename, sizeof(filename), "%d.txt", ++i);
-            fd2 = open(filename, O_WRONLY | O_CREAT, 0664);
-            if (fd2 == -1) {
-                perror("Error opening file");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-    if (!line.empty()) {
-        write(fd2, line.c_str(), line.size());
-    }
-    close(fd2);
+    /////////////////////////
 
-    if (i <= 1) {
-        close(fd);
-        return 0;
-    }
 
+    int lines_count = 2;
+    bool calc_lines = true;
     std::vector<std::string> arr;
-    int fd3, fd4;
-    for (int n = 0; n < i - 1; n++) {
-        for (int j = 0; j < i - n - 1; j++) {
-            arr.clear();
-            fd3 = open_fd(j);
-            fd4 = open_fd(j + 1);
-            arr.push_back(read_from(fd3));
-            arr.push_back(read_from(fd4));
+    std::string tmp;
+    bool swapped = false;
+
+    for (int i = 0; i < lines_count - 1; i++) {
+        swapped = false;
+
+        lseek(fd, 0, SEEK_SET);
+        lseek(fd_3, 0, SEEK_SET);
+        ftruncate(fd_3, 0);
+        arr.push_back(getLine(fd));
+
+        for (int j = 0; j < lines_count  - 1; j++) {
+            arr.push_back(getLine(fd));
+            tmp = arr[0];
 
             std::sort(arr.begin(), arr.end());
-            write_to(fd3, arr[0]);
-            write_to(fd4, arr[1]);
+            write_to(fd_3, arr[0]);
+            if(tmp != arr[0]) {
+                swapped = true;
+            }
 
-            close(fd3);
-            close(fd4);
+            arr[0] = arr[1];
+            arr.pop_back();
+
+            if(calc_lines) {
+                if (isFileEnd(fd)) {
+                    calc_lines = false;
+                    break;
+                } else {
+                    lines_count += 2;
+                }
+            }
         }
-    }
 
-// -- final file --
-    std::string str;
-    lseek(fd, 0, SEEK_SET);
-    ftruncate(fd, 0);
-    for (int j = 0; j <= i; j++) {
-      fd2 = open_fd(j);
-      str = read_from(fd2);
+        write_to(fd_3, arr[0]);
+        arr.pop_back();
 
-      write_to(fd, str, false);
-      close(fd2);
+        if(!swapped) { break;}
 
+        std::swap(fd, fd_3);
     }
 
     close(fd);
+    close(fd_3);
     return 0;
 }
