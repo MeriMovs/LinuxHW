dab7ebc1f76a5986262b356a2af47611dd0bb58d
MeriMovs <myblackbounty@gmail.com>
2025-12-11 23:43:01 +0400

    added IPC-3

---
+++ IPC-3/s.cpp
@@ -0,0 +1,26 @@
+#include "shared.h"
+#include <iostream>
+#include <unistd.h>
+#include <pthread.h>
+
+int main(){
+    shared_array arr("arr", 10);
+    sem_t *sem = arr.get_semaphore();
+
+    while(1){
+        sem_wait(sem);
+
+        std::cout << "[second] read: ";
+        for(size_t i = 0; i < arr.size(); ++i){
+            int v = arr[i];
+            std::cout << v << " ";
+            arr[i] = v + 14;
+        }
+        std::cout << "\n";
+
+        sem_post(sem);
+
+        sleep(2);
+    }
+    return 0;
+}
